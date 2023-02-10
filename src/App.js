20a6ece1bf711183051198a7583d61278120e9c6
Nika <nikaonime@gmail.com>
2023-02-10 12:03:45 +0200

    add components: Header, AddTodo, Todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import AddTodo from './Components/AddTodo/AddTodo';
+import Header from './Components/Header/Header';
+import TodoList from './Components/TodoList/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AddTodo />
+      <TodoList/>
     </div>
   );
 }
