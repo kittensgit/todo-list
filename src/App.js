7734ad644bdc0d6a80425e46570e37660399fb71
Nika <nikaonime@gmail.com>
2023-02-11 11:29:29 +0200

    useState, deleteTodo, statusTodo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React, { useState } from 'react';
 import './App.css';
 import AddTodo from './Components/AddTodo/AddTodo';
 import Header from './Components/Header/Header';
 import TodoList from './Components/TodoList/TodoList';
 
 function App() {
+
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: 'first todo',
+      status: true
+    },
+    {
+      id: 2,
+      title: 'second todo',
+      status: true
+    },
+    {
+      id: 3,
+      title: 'third todo',
+      status: false
+    },
+  ])
+
   return (
     <div className="App">
       <Header />
-      <AddTodo />
-      <TodoList/>
+      <AddTodo  todo={todo} setTodo={setTodo}/>
+      <TodoList todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
